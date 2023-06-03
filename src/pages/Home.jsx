dee73dd834a2aa80117442fd879a24370b9e8072
jaymanale <jaymanale29@gmail.com>
2023-06-03 17:17:32 +0530

    Plan trip section add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import PlanTrip from "../components/PlanTrip";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <PlanTrip />
     </>
   );
 };
